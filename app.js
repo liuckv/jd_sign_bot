1bfaa214e85e36e98c810b2993ed507cadd174da
liuckv <70507697+liuckv@users.noreply.github.com>
2021-02-10 09:58:10 +0800

    Update app.js

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ async function start() {
     if (fs.existsSync(path)) {
       content = fs.readFileSync(path, "utf8");
     }
-    await sendNotify("京东签到-" + new Date().toLocaleDateString(), content);
+    await sendNotify("京东签到_jd_sign_bot-" + new Date().toLocaleDateString(), content);
     console.log('发送结果完毕')
   }
 }
